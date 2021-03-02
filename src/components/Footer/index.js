24487ba48fb7b785043d015b437096e15e1f773e
Filip Risteski <filip22risteski@gmail.com>
2021-03-02 17:42:13 +0100

    ver 0.5

---
+++ src/components/Footer/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <FotterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About US</FooterLinkTitle>
+                                <FooterLink to="/signin">About Us</FooterLink>
+                                <FooterLink to="/signin">About Us</FooterLink>
+                                <FooterLink to="/signin">About Us</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About US</FooterLinkTitle>
+                                <FooterLink to="/signin">About Us</FooterLink>
+                                <FooterLink to="/signin">About Us</FooterLink>
+                                <FooterLink to="/signin">About Us</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+            </FooterWrap>
+        </FotterContainer>
+    )
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import HeroSection from '../components/HeroSection'
 import InfoSection from '../components/InfoSection'
 import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data'
 import Services from '../components/Services'
+import Footer from '../components/Footer'
 
 
 function Home() {
@@ -23,6 +24,7 @@ function Home() {
         <InfoSection {...homeObjTwo} />
         <Services />
         <InfoSection {...homeObjThree} />
+        <Footer />
         </>
     )
 }
